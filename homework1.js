a446d49aa72d8f570db2712253f6190c2226c5c8
Nattharavudh Snitmatjaro <nattharavudhs.@Nattharavudhs-MacBook-Pro.local>
2023-07-27 16:35:07 +0700

    hw1.1-1.5

---
+++ homework1.js
@@ -0,0 +1,127 @@
+// HW1.1
+console.log("---------- Homework 1.1 ----------")
+const draw1 = function(n){
+    let text = "";
+    for ( let i=0; i<n; i++){
+        text += "*";
+
+    }
+    console.log(text);
+}
+
+draw1(2);
+draw1(3);
+draw1(4);
+
+
+// HW1.2
+console.log("---------- Homework 1.2 ----------")
+const draw2 = function(n){
+    let text = "";
+    for ( let i=0; i<n; i++){
+        for ( let j=0; j<n; j++){
+        text += "*";}
+        text += "\n";
+    }
+    console.log(text);
+}
+
+draw2(2);
+draw2(3);
+draw2(4);
+
+
+//HW1.3
+console.log("---------- Homework 1.3 ----------")
+const draw3 = function(n){
+    let text = "";
+    for ( let i=0; i<n; i++){
+        for ( let j=0; j<n; j++){
+        text += j+1;}
+        text += "\n";
+    }
+    console.log(text);
+}
+
+draw3(2);
+draw3(3);
+draw3(4);
+
+
+//HW1.4
+console.log("---------- Homework 1.4 ----------")
+const draw4 = function(n){
+    let text = "";
+    for ( let i=0; i<n; i++){
+        for ( let j=0; j<n; j++){
+        text += i+1;}
+        text += "\n";
+    }
+    console.log(text);
+}
+
+draw4(2);
+draw4(3);
+draw4(4);
+
+
+
+//HW1.5
+console.log("---------- Homework 1.5 ----------")
+const draw5 = function(n){
+    let text = "";
+    for ( let i=0; i<n; i++){
+        for ( let j=0; j<n; j--){
+        text += i+1;}
+        text += "\n";
+    }
+    console.log(text);
+}
+
+draw5(2);
+draw5(3);
+draw5(4);
+
+
+
+//HW1.6
+console.log("---------- Homework 1.6 ----------")
+
+
+
+
+//HW1.7
+console.log("---------- Homework 1.7 ----------")
+
+
+
+
+//HW1.8
+console.log("---------- Homework 1.8 ----------")
+
+
+
+
+
+//HW1.9
+console.log("---------- Homework 1.9 ----------")
+
+
+
+
+
+//HW1.10
+console.log("---------- Homework 1.10 ----------")
+
+
+
+
+//HW1.11
+console.log("---------- Homework 1.11 ----------")
+
+
+
+
+
+//HW1.12
+console.log("---------- Homework 1.12 ----------")
